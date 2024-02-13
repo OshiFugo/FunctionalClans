2b6b7483bd2ef87900b410258b394611b06a628f
unknown <macs.topuk@gmail.com>
2024-02-13 22:18:32 +0300

    2.1.5
    Fix a bug with the addition of members to the clan
    
    Fixed it: beacmc

--- src/main/java/ru/oshifugo/functionalclans/sql/InviteRequest.java
+++ src/main/java/ru/oshifugo/functionalclans/sql/InviteRequest.java
@@ -8,12 +8,12 @@ public class InviteRequest {
         request.put(player, Clan.getUID(clanName));
     }
     public static void acceptRequest(String player) {
-            String name = Clan.getClanNameUID(InviteRequest.request.get(player));
-            String[] member = {player, "1", name, "0", "0", "0", "0", "0"};
-            SQLiteUtility.members.put(player, member);
-            SQLiteUtility.member_clan.put(player, name);
-            SQLite.execute("INSERT INTO clan_members (name, role, clan, kills, death, quest, rating) VALUES ('" + player + "', '1', '" + name + "', '0', '0', '0', '0')");
-            request.remove(player);
+        String name = Clan.getClanNameUID(InviteRequest.request.get(player));
+        String[] member = {player, "1", name, "0", "0", "0", "0", "0"};
+        SQLiteUtility.members.put(player, member);
+        SQLiteUtility.member_clan.put(player, name);
+        SQLite.execute("INSERT INTO clan_members (id, name, role, clan, kills, death, quest, rating) VALUES ('" + InviteRequest.request.get(player) + "', '" + player + "', '1', '" + name + "', '0', '0', '0', '0')");
+        request.remove(player);
     }
     public static void denyRequest(String player) {
         request.remove(player);
